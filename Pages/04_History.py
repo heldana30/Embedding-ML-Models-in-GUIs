146576772096cbb4b2088c013b496579b4639b5a
Heldana Natnael <nheldana8@gmail.com>
2024-06-23 13:18:36 +0300

    adding config file and authentication on each of the pages

--- Pages/04_History.py
+++ Pages/04_History.py
@@ -7,18 +7,21 @@ st.set_page_config(
     page_icon='🔮',
     layout='wide'
 )
+if 'authentication_status' in st.session_state and st.session_state['authentication_status']:
+    def display_history_prediction():
 
-def display_history_prediction():
+        csv_path = "Data/history.csv"
+        csv_exists = os.path.exists(csv_path)
 
-    csv_path = "Data/history.csv"
-    csv_exists = os.path.exists(csv_path)
-
-    if csv_exists:
-        history = pd.read_csv(csv_path)
-        history = history.drop('Unnamed: 0', axis =1)
-        st.dataframe(history)
+        if csv_exists:
+            history = pd.read_csv(csv_path)
+            history = history.drop('Unnamed: 0', axis =1)
+            st.dataframe(history)
 
 
-if __name__ == '__main__':
-    st.title('History Page')
-    display_history_prediction()
+    if __name__ == '__main__':
+        st.title('History Page')
+        display_history_prediction()
+
+else:
+    st.warning('Please login to access this page')
